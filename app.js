ca798779c4a3f5f70a1e9dbcfb7982a80ed288e6
dong.keke <dong.keke@tongbanjie.com>
2016-09-28 08:35:56 +0800

    添加我的女神，我们做朋友吧

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var redis = require("./models/redis");
 var bodyParser = require("body-parser");
+var mongodb = require("./models/mongodb");
 
 var app = express();
 app.use(bodyParser());
@@ -43,12 +44,47 @@ app.get("/",function(req,res){
     });
   }
   redis.pick(req.query,function(result){
+    if (result.code===1) {
+      mongodb.save(req.query.user,result.msg,function(err){
+        if (err) {
+          return res.json({
+            code:0,
+            msg:"获取漂流瓶失败，请重试"
+          });
+        }
+        return res.json(result);
+      });
+    }
     res.json(result);
   });
 });
 
 app.post("/back",function(req,res){
   redis.throwBack(req.body,function(result){
+    res.json(result);
+  });
+});
+
+app.get("/user/:user",function(req,res){
+  mongodb.getAll(req.params.user,function(result){
+    res.json(result);
+  });
+});
+
+app.get("/bottle/:_id",function(req,res){
+  mongodb.getOne(req.params._id,function(result){
+    res.json(result);
+  });
+});
+
+app.post("/reply/:_id",function(req,res){
+  if (!(req.body.user && req.body.content)) {
+    return callback({
+      code:0,
+      msg:"回复信息不完整！"
+    });
+  }
+  mongodb.reply(req.params._id,req.body,function(result){
     res.json(result);
   });
 });
